c8f472cb4ab681425a32f50928273f21a0db4b31
José Uscanga <jousmo@outlook.com>
2022-10-23 21:42:59 -0500

    feat: obtain port env variable in main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
@@ -15,5 +16,7 @@ import { ValidationPipe } from '@nestjs/common';
       },
     }),
   );
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
 })();
