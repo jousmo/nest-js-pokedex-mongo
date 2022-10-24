155ebf3772d55d806bc60d461a892ad0a6651822
José Uscanga <jousmo@outlook.com>
2022-10-23 21:43:20 -0500

    feat: change connection hardcode with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,30 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedsModule } from './seeds/seeds.module';
+import { EnvConfig } from './config/env.config';
+import { JoiConfig } from './config/joi.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfig],
+      validationSchema: JoiConfig,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://root:root@localhost:27017', {
-      dbName: 'pokemon',
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DB_URL'),
+        dbName: configService.get('DB_NAME'),
+      }),
+      inject: [ConfigService],
     }),
     PokemonModule,
     CommonModule,
